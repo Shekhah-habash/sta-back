1fc40d620fa51654540c25a9bfc963c21925d1e1
Shekhah-habash <shosho2001h.b@gmail.com>
2025-12-16 08:22:29 +0300

    Add-provider-img

--- app/helpers.php
+++ app/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Helpers\ApiResponse;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Image;
 
 // Define helper functions for ApiResponse
 if (!function_exists('apiSuccess')) {
@@ -36,5 +38,18 @@ if (!function_exists('apiUnauthorized')) {
     function apiUnauthorized(string $message = 'Unauthorized')
     {
         return ApiResponse::unauthorized($message);
+    }
+}
+
+
+if (!function_exists('saveImg')) {
+    function saveImg($type, $file)
+    {
+        // $filename = $file->store("images/$type", 'public');
+        $filename = Storage::disk('public')->putFile("images/$type", $file);
+        return Image::create([
+            'name' => $filename,
+            'type' => $type,
+        ])->id;
     }
 }
